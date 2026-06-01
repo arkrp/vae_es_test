9cf4d635b910dc5e9d2d79ae78178e73f6193e14
Hannah Nelson <nexec64@gmail.com>
2026-06-01 03:04:52 +0000

    deleted stuff. A fresh start is needed due to the required structure.

--- app/main.py
+++ app/main.py
@@ -1,9 +1,9 @@
 print("Hello World!")
 import torch
 import torchvision
-from Model import TrainingVAE, Decoder, Encoder
-from Train import BasicTraining
+from Datasets import MNIST
 print("preload complete")
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print("device loaded:" + repr(device))
+
 print("Serpent praise")
