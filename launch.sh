e537919979069c12baa5db672373bf2fe6665788
Hannah Nelson <nexec64@gmail.com>
2026-06-06 22:09:01 +0000

    got podman container properly calling cuda.

--- launch.sh
+++ launch.sh
@@ -1,3 +1,22 @@
 podman container rm vae_es_test
 podman build -t vae_es_test .
-podman run -it --device /dev/kfd --device /dev/dri --security-opt seccomp=unconfined --name vae_es_test vae_es_test $1
+podman run -it\
+    --privileged \
+    --net=host \
+    --pid=host \
+    --ipc=host \
+    --uts=host \
+    --cgroupns=host \
+    --device /dev/kfd:/dev/kfd \
+    --device /dev/dri:/dev/dri \
+    --group-add keep-groups \
+    --runtime crun \
+    --name vae_es_test \
+    vae_es_test $1
+#--cap-add=SYS_PTRACE \
+#--security-opt apparmor=unconfined \
+#--device /dev/kfd:/dev/kfd \
+#--device /dev/dri/renderD128:/dev/dri/renderD128 \
+#--device /dev/dri/card1:/dev/dri/card1 \
+#-e HSA_OVERRIDE_GFX_VERSION=11.0.0 \
+#--security-opt unmask=/sys/dev \
