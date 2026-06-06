bd80804e02a433d953fe52f736e26aeb3fc35f90
Hannah Nelson <nexec64@gmail.com>
2026-06-06 04:04:50 +0000

    made it run with podman

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM rocm/pytorch:rocm7.2.2_ubuntu24.04_py3.12_pytorch_release_2.7.1
+FROM docker.io/rocm/pytorch:rocm7.2.2_ubuntu24.04_py3.12_pytorch_release_2.7.1
 WORKDIR /app
 COPY /app /app
 CMD ["/opt/venv/bin/python", "/app/main.py"]
--- launch.sh
+++ launch.sh
@@ -1,3 +1,3 @@
-docker container rm vae_es_test
-docker build -t vae_es_test .
-docker run -it --device /dev/kfd --device /dev/dri --security-opt seccomp=unconfined --name vae_es_test vae_es_test $1
+podman container rm vae_es_test
+podman build -t vae_es_test .
+podman run -it --device /dev/kfd --device /dev/dri --security-opt seccomp=unconfined --name vae_es_test vae_es_test $1
